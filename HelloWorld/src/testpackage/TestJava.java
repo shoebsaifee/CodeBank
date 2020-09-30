77ce5f596c762f21c24270581edf277f3e72d097
shoebsaifee <shoebsaifee@gmail.com>
2020-09-30 21:26:53 +0530

    Update TestJava.java

--- HelloWorld/src/testpackage/TestJava.java
+++ HelloWorld/src/testpackage/TestJava.java
@@ -2,7 +2,7 @@ package testpackage;
 
 public class TestJava {
     public static void main(String[] args) {
-        System.out.println("Test Java");
+        System.out.println("Changed from master");
         System.out.println("Test Java!!!!");
     }
 }
